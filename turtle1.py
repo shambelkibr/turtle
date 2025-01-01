ff1716f1e4e30693c9e698c1d6731a502d1785ea
shambelkibr <shambel5110@gmail.com>
2025-01-01 06:32:07 -0800

    turtle practice

---
+++ turtle1.py
@@ -0,0 +1,22 @@
+from turtle import*
+forward(100)           """the the frist"""
+right(90)
+forward(100)
+
+
+forward(100)          # the fourth
+right(90)
+forward(100)
+right(90)
+forward(100)
+
+
+forward(100)          # the second 
+left(90)
+forward(100)
+
+forward(100)         # the thrid
+left(90)
+forward(100)
+left(90)
+forward(100)
